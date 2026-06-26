1234aca1cd152fcf4b4eabf312797f55e64d6144
Guilherme <guiilhermecd@gmail.com>
2026-06-26 09:49:27 -0300

    Project created

---
+++ backend/gcatalog/src/main/java/gcatalog/GcatalogApplication.java
@@ -0,0 +1,13 @@
+package gcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcatalogApplication.class, args);
+	}
+
+}
